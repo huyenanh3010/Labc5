b0a38a3a80f6c56f2c56b853fdee1868c4927474
huyenanh3010 <nguyenhuyenanhac2025@gmail.com>
2026-02-09 14:50:28 +0700

    Upload Lab C5

---
+++ Assets/Scrips/PlayerCollision2D.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision2D : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("ON COLLISION với: " + collision.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("ON TRIGGER với: " + other.gameObject.name);
+    }
+}
---
+++ Assets/Scrips/PlayerDropDown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerDropDown : MonoBehaviour
+{
+    private Collider2D playerCollider;
+
+    void Start()
+    {
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    void Update()
+    {
+        // Nhấn S hoặc mũi tên xuống
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            StartCoroutine(DropDown());
+        }
+    }
+
+    IEnumerator DropDown()
+    {
+        // Tắt va chạm giữa Player và OneWayPlatform
+        Physics2D.IgnoreLayerCollision(
+            LayerMask.NameToLayer("Player"),
+            LayerMask.NameToLayer("OneWayPlatform"),
+            true
+        );
+
+        yield return new WaitForSeconds(0.4f);
+
+        // Bật lại va chạm
+        Physics2D.IgnoreLayerCollision(
+            LayerMask.NameToLayer("Player"),
+            LayerMask.NameToLayer("OneWayPlatform"),
+            false
+        );
+    }
+}
